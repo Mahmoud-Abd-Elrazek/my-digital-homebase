c1a6789f0fa3489ec519e7e1002528183ed23947
Mahmoud Abdelrazek <152639174+Mahmoud-Abd-Elrazek@users.noreply.github.com>
2025-03-27 07:18:22 +0200

    add profile image link

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -57,7 +57,7 @@ const AboutSection = () => {
           >
             <div className="relative z-10 aspect-square rounded-2xl overflow-hidden">
               <BlurImage
-                src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80"
+                src="https://www.facebook.com/photo/?fbid=477600985312649&set=a.106859322386819"
                 alt="Portrait"
                 className="w-full h-full object-cover"
                 containerClassName="w-full h-full"
